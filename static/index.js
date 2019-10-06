e06cc51fe776c70245609434e1236b51023c1def
kingkingyyk <king8793yyk@hotmail.com>
2019-10-06 18:09:16 +0800

    Remove chromecast js

--- static/index.js
+++ static/index.js
@@ -34,7 +34,7 @@ function loadDirectory(folderId) {
 
 mediaCastURL = "";
 mediaCastMime = "";
-castHTML = "<button is='google-cast-button' style='height: 32px; width: 32px'></button>";
+castHTML = "";
 function loadFile(fileId, fileName, fileType, mimeType) {
     url = getCurrentURL() + "download/"+fileId;
     var previewableTypes = ["movie", "music", "picture", "code"];
@@ -172,33 +172,3 @@ $(function() {
     initUI();
     loadDirectory(rootDirectory);
 });
-
-
-window.__onGCastApiAvailable = function(isAvailable){
-    if(! isAvailable){
-        return false;
-    }
-
-    var castContext = cast.framework.CastContext.getInstance();
-
-    castContext.setOptions({
-        autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
-        receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID
-    });
-
-    var stateChanged = cast.framework.CastContextEventType.CAST_STATE_CHANGED;
-    castContext.addEventListener(stateChanged, function(event){
-        var castSession = castContext.getCurrentSession();
-        var media = new chrome.cast.media.MediaInfo(mediaCastURL, mediaCastMime);
-        var request = new chrome.cast.media.LoadRequest(media);
-
-        castSession && castSession
-            .loadMedia(request)
-            .then(function(){
-                console.log('Success');
-            })
-            .catch(function(error){
-                console.log('Error: ' + error);
-            });
-    });
-};
