d6873beefd0b8b9d3c048e5b8921319e4d90e684
kingkingyyk <king8793yyk@hotmail.com>
2020-04-26 20:13:28 +0800

    Improve dockerfile

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+export DJANGO_SETTINGS_MODULE=RPiDrive_ng.settings.prod
+python manage.py makemigrations
+python manage.py makemigrations drive
+python manage.py makemigrations mediaplayer
+python manage.py migrate
+python manage.py migrate drive
+python manage.py migrate mediaplayer
+python manage.py initialize
+python manage.py indexer
+/usr/local/bin/gunicorn -w 9 -b 0.0.0.0:8888 RPiDrive_ng.wsgi --timeout 3000 --preload
