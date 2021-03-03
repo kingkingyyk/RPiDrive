7d16e7f3ffd98319f28fd22cff36ed53a50182c8
kingkingyyk <king8793yyk@hotmail.com>
2021-03-03 22:44:04 +0800

    Update gunicorn config

--- backend/docker-start.sh
+++ backend/docker-start.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations drive --noinput && \
 python manage.py migrate drive && \
 
 python manage.py jobserver &
-gunicorn --workers=20 --bind 0.0.0.0:8000 rpidrive.wsgi
+gunicorn --workers=5 --threads=4 --bind=0.0.0.0:8000 --timeout=86400 rpidrive.wsgi
