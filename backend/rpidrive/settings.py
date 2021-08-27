9575d7306b9bd4ba3809ef8a0de546889aeede37
Yap Yee King <king8793yyk@hotmail.com>
2021-08-27 12:00:29 +0000

    Reduce CONN_MAX_AGE again

--- backend/rpidrive/settings.py
+++ backend/rpidrive/settings.py
@@ -79,7 +79,7 @@ elif DB_ENGINE == 'django.db.backends.postgresql_psycopg2':
         'NAME': CONFIG_DICT['database']['name'],
         'USER': CONFIG_DICT['database']['user'],
         'PASSWORD': CONFIG_DICT['database']['password'],
-        'CONN_MAX_AGE': 2,
+        'CONN_MAX_AGE': 1,
     }
 else:
     raise Exception('Invalid db engine!')
