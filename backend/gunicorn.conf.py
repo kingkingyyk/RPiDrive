aa0412c4c6058f24758b4f23098c9faec45d7ad7
kingkingyyk <king8793yyk@hotmail.com>
2022-06-17 13:24:08 +0000

    Update gunicorn.conf.py

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -4,7 +4,7 @@ from multiprocessing import cpu_count
 from psycogreen.gevent import patch_psycopg
 
 def post_fork(server, worker):
-    gevent.monkey.patch_all()
+    monkey.patch_all()
     patch_psycopg()
 
 bind = '0.0.0.0:8000'
